6b5d544b6d3f1c823ab198fd1f6f9c79c81a5a83
beast047 <beast047@gmail.com>
2025-04-21 22:04:08 -0500

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,7 +1,24 @@
-const buttonEl = document.getElementById('callBtn');
+function digitalClock(){
+var date = new Date();
+var hours = date.getHours() + '';
+var minutes = date.getMinutes() + '';
+var seconds = date.getSeconds() + '';
+var day = date.getDay();
+if(hours.length < 2){
+    hours = '0' + hours;
+}
+if(minutes.length < 2){
+    minutes = '0' + minutes;
+}
+if(seconds.length < 2){
+    seconds = '0' + seconds;
+}
+var weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var clock = weekDay[day] + '  ' + hours + ':' + minutes+ ':' + seconds;
+document.getElementById('clock').innerHTML = clock; 
 
-function call(){
-    alert('Calling all cars!!');
 }
 
-buttonEl.addEventListener('click', call);
+
+digitalClock();
+setInterval(digitalClock,1000);
